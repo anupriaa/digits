745556d91f95c2df7f9e3b6872d886d0baf25835
Anupriya Srivastava <anupriya@hawaii.edu>
2015-04-02 19:44:34 -1000

    finished

--- app/views/formdata/ContactFormData.java
+++ app/views/formdata/ContactFormData.java
@@ -1,14 +1,44 @@
 package views.formdata;
 
+import play.data.validation.ValidationError;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Backing class for the new contact form.
  * Created by Anupriya on 4/2/2015.
  */
 public class ContactFormData {
+  /** Constant to store legth of telephone. */
+  private static final int TELEPHONE_LEN = 12;
   /** String to hold first name. */
    public String firstName = "";
   /** String to hold last name. */
    public String lastName = "";
   /** String to hold telephone number of the format xxx-xxx-xxxx. */
    public String telephone = "";
+
+  /**
+   * Checks if the form fields are valid. Invoked by bindFormRequest().
+   * @return null if no errors else list of errors.
+   */
+  public List<ValidationError> validate() {
+    List<ValidationError> errors = new ArrayList<>();
+    if (firstName == null || firstName.length() == 0) {
+      errors.add(new ValidationError("firstName", "First Name is a required field"));
+    }
+    if (lastName == null || lastName.length() == 0) {
+      errors.add(new ValidationError("lastName", "Last Name is a required field"));
+    }
+    if (telephone == null || telephone.length() == 0) {
+      errors.add(new ValidationError("telephone", "Telephone is a required field"));
+    }
+    if (telephone.length() != TELEPHONE_LEN) {
+      errors.add(new ValidationError("telephone", "Telephone must be xxx-xxx-xxxx"));
+    }
+    return errors.isEmpty() ? null : errors;
+  }
 }
+
+
