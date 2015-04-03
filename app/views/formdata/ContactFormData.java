5dad29159aff17d0296b232ff0b2bc5515bd6266
Anupriya Srivastava <anupriya@hawaii.edu>
2015-04-02 15:27:08 -1000

    fixed

---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,11 @@
+package views.formdata;
+
+/**
+ * Provides the backing class for the NewContact page form
+ * Created by Anupriya on 4/2/2015.
+ */
+public class ContactFormData {
+  public String firstName = "";
+  public String lastName = "";
+  public String telephone = "";
+}
