b54a1d288fd82c4032ceb654bd2d1141dd712895
Anupriya Srivastava <anupriya@hawaii.edu>
2015-04-05 21:49:35 -1000

    finished

--- app/Global.java
+++ app/Global.java
@@ -12,7 +12,6 @@ public class Global extends GlobalSettings {
   @Override
   public void onStart(Application application) {
     super.onStart(application);
-    Logger.info("Application has started");
     ContactDB.addContact(new ContactFormData("Anu", "Sinha", "789-789-7869", "Home"));
     ContactDB.addContact(new ContactFormData("Bharat", "Sinha", "789-789-7899", "Home"));
     ContactDB.addContact(new ContactFormData("Shivam", "Srivastava", "783-789-7899", "Home"));
